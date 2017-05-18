5c6262fd5765fbc283bdbf56d7572806ff100465
sairoutine <sairoutine@gmail.com>
2017-05-18 16:58:46 +0900

    update: use setupEvents method

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,6 @@
 'use strict';
 var Game = require('./game');
 
-// WebAudio
-window.AudioContext = window.AudioContext || window.webkitAudioContext;
-
 var game;
 
 window.onload = function() {
@@ -11,26 +8,12 @@ window.onload = function() {
 	var mainCanvas = document.getElementById('mainCanvas');
 	// Game オブジェクト
 	game = new Game(mainCanvas);
+
+	// イベントハンドラの設定
+	game.setupEvents();
+
 	// 初期化
 	game.init();
-
-	// フォントの読み込みが完了
-	if(document.fonts) {
-		document.fonts.addEventListener('loadingdone', function() { game.fontLoadingDone(); });
-	}
-	else {
-		// フォントロードに対応してなければ無視
-		game.fontLoadingDone();
-	}
-
-	// キーバインド
-	window.onkeydown = function(e) { game.handleKeyDown(e); };
-	window.onkeyup   = function(e) { game.handleKeyUp(e); };
-
-	// ゲームパッド
-	if(window.Gamepad && navigator.getGamepads) {
-		game.enableGamePad();
-	}
 
 	// ゲーム起動
 	game.startRun();
