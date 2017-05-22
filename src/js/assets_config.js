662d2245e364d590373525a3e76d193fe281c9a6
sairoutine <sairoutine@gmail.com>
2017-05-23 01:05:24 +0900

    add: images

---
+++ src/js/assets_config.js
@@ -0,0 +1,17 @@
+'use strict';
+
+var AssetsConfig = {};
+AssetsConfig.images = {
+	jolicraft:  "./image/jolicraft.png",
+	ldfaithful: "./image/ldfaithful.png",
+	oryx:       "./image/oryx.png",
+};
+
+AssetsConfig.sounds = {
+};
+
+AssetsConfig.bgms = {
+};
+
+
+module.exports = AssetsConfig;
