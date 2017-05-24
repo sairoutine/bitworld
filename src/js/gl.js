c499d54941c5b29ddf4f1cf06d587f8a1a7a95e9
sairoutine <sairoutine@gmail.com>
2017-05-24 10:39:46 +0900

    update gl class

--- src/js/gl.js
+++ src/js/gl.js
@@ -3,14 +3,18 @@
 var WebGLDebugUtils = require("webgl-debug");
 
 var createWebGLContext = function (canvas) {
+	//  rewrite canvas dom
+	canvas = document.createElement('canvas');
 	var gl;
 	try {
-		gl = WebGLDebugUtils.makeDebugContext(canvas.getContext("experimental-webgl"));
+		gl = canvas.getContext('webgl') || canvas.getContext('experimental-webgl');
+		gl = WebGLDebugUtils.makeDebugContext(gl);
 	} catch (e) {
 		window.alert(e);
 	}
-	if (!gl) 
+	if (!gl) {
 		window.alert("Could not initialize WebGL");
+	}
 
 	return gl;
 };
