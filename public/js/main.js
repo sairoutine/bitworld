9f81efb0fb6d41dc969f1b482613d8cddd970c08
sairoutine <sairoutine@gmail.com>
2017-05-27 21:35:06 +0900

    compile js

--- public/js/main.js
+++ public/js/main.js
@@ -7880,6 +7880,7 @@ module.exports = AssetsConfig;
 },{}],16:[function(require,module,exports){
 'use strict';
 var glmatrix = require("gl-matrix");
+var Util = require('./hakurei').util;
 
 var Camera = function() {
 	this.matrix = glmatrix.mat4.create();
@@ -7917,22 +7918,22 @@ Camera.prototype.changeAngle = function(dTheta) {
 	this.theta[0] -= dTheta[0];
 	this.theta[1] -= dTheta[1];
 	this.theta[2] -= dTheta[2];
-	this.theta[0] = this.theta[0].clamp(this.thetaLimits[0],this.thetaLimits[1]);
+	this.theta[0] = Util.clamp(this.theta[0], this.thetaLimits[0],this.thetaLimits[1]);
 };
 
 Camera.prototype.setAngle = function(theta) {
 	this.theta = theta;
-	this.theta[0] = this.theta[0].clamp(this.thetaLimits[0],this.thetaLimits[1]);
+	this.theta[0] = Util.clamp(this.theta[0], this.thetaLimits[0],this.thetaLimits[1]);
 };
 
 Camera.prototype.changeDistance = function(amount) {
 	this.desiredDistance += amount;
-	this.desiredDistance = this.desiredDistance.clamp(this.distanceLimits[0],this.distanceLimits[1]);
+	this.desiredDistance = Util.clamp(this.desiredDistance, this.distanceLimits[0],this.distanceLimits[1]);
 };
 
 Camera.prototype.setDistance = function(dist) {
 	this.desiredDistance = dist;
-	this.desiredDistance = this.desiredDistance.clamp(this.distanceLimits[0],this.distanceLimits[1]);
+	this.desiredDistance = Util.clamp(this.desiredDistance, this.distanceLimits[0],this.distanceLimits[1]);
 };
 
 Camera.prototype.sphericalToCartesian = function(origin,r,angles) {
@@ -7961,7 +7962,7 @@ Camera.prototype.updateMatrix = function(env) {
 
 module.exports = Camera;
 
-},{"gl-matrix":2}],17:[function(require,module,exports){
+},{"./hakurei":24,"gl-matrix":2}],17:[function(require,module,exports){
 'use strict';
 var DEBUG = require("./debug_constant");
 
@@ -8700,14 +8701,27 @@ module.exports = Constant;
 
 },{}],29:[function(require,module,exports){
 'use strict';
+var WebGLDebugUtils = require("webgl-debug");
 var CONSTANT = require("./constant");
 var ImageLoader = require("./asset_loader/image");
 var AudioLoader = require("./asset_loader/audio");
 var FontLoader = require("./asset_loader/font");
 
-var Core = function(canvas) {
+var Core = function(canvas, options) {
+	if(!options) {
+		options = {};
+	}
+
 	this.canvas_dom = canvas;
-	this.ctx = this.canvas_dom.getContext('2d');
+	this.ctx = null; // 2D context
+	this.gl  = null; // 3D context
+
+	if(options.webgl) {
+		this.gl = this.createWebGLContext(this.canvas_dom);
+	}
+	else {
+		this.ctx = this.canvas_dom.getContext('2d');
+	}
 
 	this.width = Number(canvas.getAttribute('width'));
 	this.height = Number(canvas.getAttribute('height'));
@@ -8819,7 +8833,20 @@ Core.prototype.changeNextSceneIfReserved = function() {
 	}
 };
 Core.prototype.clearCanvas = function() {
-	this.ctx.clearRect(0, 0, this.width, this.height);
+	if (this.is2D()) {
+		// 2D
+		this.ctx.clearRect(0, 0, this.width, this.height);
+	}
+	else if (this.is3D()) {
+		// 3D
+		// TODO:
+	}
+};
+Core.prototype.is2D = function() {
+	return this.ctx ? true : false;
+};
+Core.prototype.is3D = function() {
+	return this.gl ? true : false;
 };
 Core.prototype.handleKeyDown = function(e) {
 	this.current_keyflag |= this._keyCodeToBitCode(e.keyCode);
@@ -8948,10 +8975,29 @@ Core.prototype.setupEvents = function() {
 	}
 };
 
+Core.prototype.createWebGLContext = function(canvas) {
+	var gl;
+	try {
+		gl = canvas.getContext('webgl') || canvas.getContext('experimental-webgl');
+		gl = WebGLDebugUtils.makeDebugContext(gl);
+	} catch (e) {
+		throw e;
+	}
+	if (!gl) {
+		throw new Error ("Could not initialize WebGL");
+	}
+
+	return gl;
+};
+
+
+
+
+
 
 module.exports = Core;
 
-},{"./asset_loader/audio":25,"./asset_loader/font":26,"./asset_loader/image":27,"./constant":28}],30:[function(require,module,exports){
+},{"./asset_loader/audio":25,"./asset_loader/font":26,"./asset_loader/image":27,"./constant":28,"webgl-debug":14}],30:[function(require,module,exports){
 'use strict';
 module.exports = {
 	util: require("./util"),
@@ -9914,6 +9960,9 @@ var Util = {
 
 		return 'rgb(' + r + ', ' + g + ', ' + b + ')';
 	},
+	clamp: function(num, min, max) {
+		return (num < min ? min : (num > max ? max : num));
+	},
 };
 
 module.exports = Util;
@@ -9955,7 +10004,9 @@ window.onload = function() {
 	// Canvas
 	var mainCanvas = document.getElementById('mainCanvas');
 	// Game オブジェクト
-	game = new Game(mainCanvas);
+	game = new Game(mainCanvas, {
+		webgl: true,
+	});
 
 	// イベントハンドラの設定
 	game.setupEvents();
@@ -10195,6 +10246,8 @@ SceneLoading.prototype.draw = function(){
 	base_scene.prototype.draw.apply(this, arguments);
 	var ctx = this.core.ctx;
 
+	if(!ctx) return; // 2D context has been depricated in this game
+
 	// 背景
 	ctx.save();
 	ctx.fillStyle = 'white';
@@ -10245,10 +10298,26 @@ module.exports = SceneLoading;
 },{"../assets_config":15,"../hakurei":24}],44:[function(require,module,exports){
 'use strict';
 
+/*
+ * TODO
+ * createData →リファクタ
+ * 各種オブジェクトのリファクタ
+  camera.js
+  data.js
+  dungeon.js
+  dungeon_convert.js
+  gl.js
+  level.js
+  point_light.js
+  programs.js
+  sprite.js
+  sprites.js
+  terrain.js
+  texture.js
+  scene/stage.js
+ */
+
 // utils
-Number.prototype.clamp = function(min, max) {
-	return (this < min ? min : (this > max ? max : this));
-};
 function centerXY(pos) {
 	return [pos[0]+0.5, pos[1]+0.5, pos[2]];
 }
@@ -10279,19 +10348,13 @@ util.inherit(SceneLoading, base_scene);
 SceneLoading.prototype.init = function() {
 	base_scene.prototype.init.apply(this, arguments);
 
-	this.canvas = document.getElementById('subCanvas');
-	this.gl = createWebGLContext(this.canvas);
+	this.data = createData(this.core.gl);
 
-	this.data = createData(this.gl);
-
-	var texture = {};
-	texture.land    = new TextureAtlas(this.gl, this.core.image_loader.getImage("ldfaithful"), 8);
-	texture.sprites = new TextureAtlas(this.gl, this.core.image_loader.getImage("oryx"), 8);
-
-	this.counter = 0; // TODO: frame_count に置き換える
+	var land    = new TextureAtlas(this.core.gl, this.core.image_loader.getImage("ldfaithful"), 8);
+	var sprites = new TextureAtlas(this.core.gl, this.core.image_loader.getImage("oryx"), 8);
 
 	// 地形
-	this.terrain = new Terrain(this.gl, texture.land);
+	this.terrain = new Terrain(this.core.gl, land);
 	// カメラ
 	this.camera = new Camera();
 
@@ -10305,7 +10368,7 @@ SceneLoading.prototype.init = function() {
 	this.lights[0] = new PointLight([1.0, 0.5, 0.0], [0,0,1], [0.3, 0.1, 0.05]);
 
 
-	this.sprites = new Sprites(this.gl, texture.sprites);
+	this.sprites = new Sprites(this.core.gl, sprites);
 	this.sprites.addSprite(Math.floor(Math.random()*256), [0,0,0]);
 
 	// プレイヤー
@@ -10340,11 +10403,11 @@ SceneLoading.prototype.beforeDraw = function() {
 
 };
 SceneLoading.prototype.draw = function(){
-	this.gl.clearColor.apply(this,this.data.background);
-	this.gl.clear(this.gl.COLOR_BUFFER_BIT|this.gl.DEPTH_BUFFER_BIT);
+	this.core.gl.clearColor.apply(this,this.data.background);
+	this.core.gl.clear(this.core.gl.COLOR_BUFFER_BIT|this.core.gl.DEPTH_BUFFER_BIT);
 
-	this.gl.viewport(0, 0, this.canvas.width, this.canvas.height);
-	glmat.mat4.perspective(this.data.world.m.pMatrix, 45.0, this.canvas.width/this.canvas.height, 0.1, 100.0);
+	this.core.gl.viewport(0, 0, this.core.width, this.core.height);
+	glmat.mat4.perspective(this.data.world.m.pMatrix, 45.0, this.core.width/this.core.height, 0.1, 100.0);
 
 	this.handleInputs();
 	//handleInputs();
@@ -10362,16 +10425,16 @@ SceneLoading.prototype.draw = function(){
 };
 
 SceneLoading.prototype.renderWorld = function(){
-	this.gl.enable(this.gl.CULL_FACE);
-	this.gl.cullFace(this.gl.BACK);
-	this.gl.useProgram(this.data.world.program);
+	this.core.gl.enable(this.core.gl.CULL_FACE);
+	this.core.gl.cullFace(this.core.gl.BACK);
+	this.core.gl.useProgram(this.data.world.program);
 	this.data.world.m.vMatrix = this.camera.matrix;
 
-	this.gl.uniformMatrix4fv(this.data.world.u.MMatrix, false, this.data.world.m.mMatrix);
-	this.gl.uniformMatrix4fv(this.data.world.u.VMatrix, false, this.data.world.m.vMatrix);
-	this.gl.uniformMatrix4fv(this.data.world.u.PMatrix, false, this.data.world.m.pMatrix);
+	this.core.gl.uniformMatrix4fv(this.data.world.u.MMatrix, false, this.data.world.m.mMatrix);
+	this.core.gl.uniformMatrix4fv(this.data.world.u.VMatrix, false, this.data.world.m.vMatrix);
+	this.core.gl.uniformMatrix4fv(this.data.world.u.PMatrix, false, this.data.world.m.pMatrix);
 
-	this.gl.uniform3fv(this.data.world.u.AmbientColor, this.level.ambient);
+	this.core.gl.uniform3fv(this.data.world.u.AmbientColor, this.level.ambient);
 
 	this.updateLights(this.data.world);
 	// Bind buffers
@@ -10379,49 +10442,48 @@ SceneLoading.prototype.renderWorld = function(){
 	this.attribSetup(this.data.world.a.Texture, this.terrain.texCoordObject, 2);
 	this.attribSetup(this.data.world.a.Normal, this.terrain.normalObject, 3);
 
-	this.gl.activeTexture(this.gl.TEXTURE0);
-	this.gl.bindTexture(this.gl.TEXTURE_2D, this.terrain.textureAtlas.texture);
-	this.gl.uniform1i(this.data.world.u.Sampler, 0);
+	this.core.gl.activeTexture(this.core.gl.TEXTURE0);
+	this.core.gl.bindTexture(this.core.gl.TEXTURE_2D, this.terrain.textureAtlas.texture);
+	this.core.gl.uniform1i(this.data.world.u.Sampler, 0);
 
-	this.gl.bindBuffer(this.gl.ELEMENT_ARRAY_BUFFER, this.terrain.indexObject);
-	this.gl.drawElements(this.gl.TRIANGLES, this.terrain.numVertices(), this.gl.UNSIGNED_SHORT, 0);
+	this.core.gl.bindBuffer(this.core.gl.ELEMENT_ARRAY_BUFFER, this.terrain.indexObject);
+	this.core.gl.drawElements(this.core.gl.TRIANGLES, this.terrain.numVertices(), this.core.gl.UNSIGNED_SHORT, 0);
 };
 
 SceneLoading.prototype.updateLights = function(program){
 	for (var i=0; i<this.lights.length; i++) {
 		this.lights[i].update();
-		this.gl.uniform1f(program.u.Light[i].enabled, this.lights[i].enabled);
-		this.gl.uniform3fv(program.u.Light[i].attenuation, this.lights[i].attenuation);
-		this.gl.uniform3fv(program.u.Light[i].color, this.lights[i].color);
-		this.gl.uniform3fv(program.u.Light[i].position, this.lights[i].position);
+		this.core.gl.uniform1f(program.u.Light[i].enabled, this.lights[i].enabled);
+		this.core.gl.uniform3fv(program.u.Light[i].attenuation, this.lights[i].attenuation);
+		this.core.gl.uniform3fv(program.u.Light[i].color, this.lights[i].color);
+		this.core.gl.uniform3fv(program.u.Light[i].position, this.lights[i].position);
 	}
 };
 
 SceneLoading.prototype.attribSetup = function(attrib, object, size, type) {
 	if (!type)
-		type = this.gl.FLOAT;
-	this.gl.enableVertexAttribArray(attrib);
-	this.gl.bindBuffer(this.gl.ARRAY_BUFFER, object);
-	this.gl.vertexAttribPointer(attrib, size, type, false, 0, 0);
+		type = this.core.gl.FLOAT;
+	this.core.gl.enableVertexAttribArray(attrib);
+	this.core.gl.bindBuffer(this.core.gl.ARRAY_BUFFER, object);
+	this.core.gl.vertexAttribPointer(attrib, size, type, false, 0, 0);
 };
 
 SceneLoading.prototype.renderSprites = function() {
-	this.gl.disable(this.gl.CULL_FACE);
-	this.counter++;
+	this.core.gl.disable(this.core.gl.CULL_FACE);
 
-	this.gl.useProgram(this.data.sprites.program);
+	this.core.gl.useProgram(this.data.sprites.program);
 	this.data.world.m.vMatrix = this.camera.matrix;
 
 	this.sprites.sprites[0].theta = this.camera.theta[2];
 	this.sprites.update();
 
-	this.gl.uniformMatrix4fv(this.data.sprites.u.MMatrix, false, this.data.world.m.mMatrix);
-	this.gl.uniformMatrix4fv(this.data.sprites.u.VMatrix, false, this.data.world.m.vMatrix);
-	this.gl.uniformMatrix4fv(this.data.sprites.u.PMatrix, false, this.data.world.m.pMatrix);
+	this.core.gl.uniformMatrix4fv(this.data.sprites.u.MMatrix, false, this.data.world.m.mMatrix);
+	this.core.gl.uniformMatrix4fv(this.data.sprites.u.VMatrix, false, this.data.world.m.vMatrix);
+	this.core.gl.uniformMatrix4fv(this.data.sprites.u.PMatrix, false, this.data.world.m.pMatrix);
 
-	this.gl.uniform1f(this.data.sprites.u.Counter, this.counter);
-	this.gl.uniform3fv(this.data.sprites.u.AmbientColor, this.level.ambient);
-	this.gl.uniform3fv(this.data.sprites.u.CamPos, this.camera.pos);
+	this.core.gl.uniform1f(this.data.sprites.u.Counter, this.frame_count);
+	this.core.gl.uniform3fv(this.data.sprites.u.AmbientColor, this.level.ambient);
+	this.core.gl.uniform3fv(this.data.sprites.u.CamPos, this.camera.pos);
 
 	this.updateLights(this.data.sprites);
 
@@ -10432,12 +10494,12 @@ SceneLoading.prototype.renderSprites = function() {
 	this.attribSetup(this.data.sprites.a.Moving, this.sprites.movingObject, 1);
 	this.attribSetup(this.data.sprites.a.Flipped, this.sprites.flippedObject, 1);
 
-	this.gl.activeTexture(this.gl.TEXTURE0);
-	this.gl.bindTexture(this.gl.TEXTURE_2D, this.sprites.textureAtlas.texture);
-	this.gl.uniform1i(this.data.sprites.u.Sampler, 0);
+	this.core.gl.activeTexture(this.core.gl.TEXTURE0);
+	this.core.gl.bindTexture(this.core.gl.TEXTURE_2D, this.sprites.textureAtlas.texture);
+	this.core.gl.uniform1i(this.data.sprites.u.Sampler, 0);
 
-	this.gl.bindBuffer(this.gl.ELEMENT_ARRAY_BUFFER, this.sprites.indexObject);
-	this.gl.drawElements(this.gl.TRIANGLES, this.sprites.numVertices(), this.gl.UNSIGNED_SHORT, 0);
+	this.core.gl.bindBuffer(this.core.gl.ELEMENT_ARRAY_BUFFER, this.sprites.indexObject);
+	this.core.gl.drawElements(this.core.gl.TRIANGLES, this.sprites.numVertices(), this.core.gl.UNSIGNED_SHORT, 0);
 };
 
 SceneLoading.prototype.checkStairs = function() {
